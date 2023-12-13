43bce8b25e806a84eaf487e9ece42298ace2096a
xola-mthembu <xola.mthembu@gmail.com>
2023-12-13 05:40:26 -0800

    Task 1 updated

--- shell_v0.1.c
+++ shell_v0.1.c
@@ -7,20 +7,46 @@
 #define MAX_COMMAND_LENGTH 100
 
 /**
- * main - Entry point for the simple shell
+ * execute_command - Executes a single-word command
+ * @command: The command to execute
  *
- * Description: A simple UNIX command interpreter that reads commands from
- * standard input and executes them. The shell runs in a loop, displaying
- * a prompt and waiting for user input. It handles single-word commands
- * without arguments. When EOF (Ctrl+D) is encountered, the shell exits.
+ * Return: void
+ */
+void execute_command(char *command)
+{
+	char *argv[2];
+	int status;
+
+	argv[0] = command; /* Set up arguments for execve */
+	argv[1] = NULL;
+
+	if (fork() == 0)
+	{
+		/* Child process */
+		if (execve(command, argv, NULL) == -1)
+		{
+			perror("./shell_v0.1");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		/* Parent process */
+		wait(&status);
+	}
+}
+/**
+ * main - Entry point for the simple shell 0.1
+ *
+ * Description: A simple UNIX command interpreter that reads single-word
+ * commands from standard input and executes them. It handles the EOF
+ * condition and prints an error if the command is not found.
  *
  * Return: 0 on successful execution, or error code on failure.
  */
 int main(void)
 {
 	char command[MAX_COMMAND_LENGTH];
-	char *argv[2];
-	int status;
 
 	while (1)
 	{
@@ -33,23 +59,14 @@ int main(void)
 		if (command[strlen(command) - 1] == '\n')
 			command[strlen(command) - 1] = '\0';  /* Remove newline character */
 
-		argv[0] = command;  /* Set up arguments for execve */
-		argv[1] = NULL;
+		/* Handling single-word commands only */
+		if (strchr(command, ' ') != NULL)
+		{
+			printf("%s: No such file or directory\n", command);
+			continue;
+		}
 
-		if (fork() == 0)
-		{
-			/* Child process */
-			if (execve(command, argv, NULL) == -1)
-			{
-				perror("./shell_v0.1");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			/* Parent process */
-			wait(&status);
-		}
+		execute_command(command);
 	}
 
 	return (0);
